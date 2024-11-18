750317af39632f40e2e382353f3df1be91b21b8d
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-11-18 20:38:37 +0800

    kernel: bypass modversion disagreements
    now gonna ship wireguard as a module
    and I dont want to rezip everytime

--- kernel/module.c
+++ kernel/module.c
@@ -1323,9 +1323,9 @@ static int check_version(const struct load_info *info,
 	return 1;
 
 bad_version:
-	pr_warn("%s: disagrees about version of symbol %s\n",
+	pr_warn("%s: disagrees about version of symbol %s (bypassed)\n",
 	       info->name, symname);
-	return 0;
+	return 1;
 }
 
 static inline int check_modstruct_version(const struct load_info *info,
@@ -3101,9 +3101,9 @@ static int check_modinfo(struct module *mod, struct load_info *info, int flags)
 		if (err)
 			return err;
 	} else if (!same_magic(modmagic, vermagic, info->index.vers)) {
-		pr_err("%s: version magic '%s' should be '%s'\n",
+		pr_err("%s: version magic '%s' should be '%s' (bypassed) \n",
 		       info->name, modmagic, vermagic);
-		return -ENOEXEC;
+		// return -ENOEXEC;
 	}
 
 	if (!get_modinfo(info, "intree")) {
