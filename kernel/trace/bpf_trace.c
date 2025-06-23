aec9171d73e310905e337fbf8a94414ee4ce31ef
Sultan Alsawaf <sultan@kerneltoast.com>
2025-06-22 20:31:04 -0700

    bpf: Decouple BPF_EVENTS from FTRACE and EVENT_TRACING
    BPF_EVENTS controls inclusion of bpf_trace, which is also responsible for
    tracepoint programs. In order to use tracepoint programs without needing to
    enable FTRACE and EVENT_TRACING, make it possible to enable BPF_EVENTS
    with just TRACEPOINTS as the only "event" type.
    
    Even though this doesn't necessarily make it possible to use a tracepoint
    program (since this doesn't require all tracepoints to be compiled), it is
    enough to make the gpuWork eBPF program load successfully.
    
    The gpuWork program is part of gpuservice in Android. When gpuWork fails to
    load, gpuservice immediately kills itself on an assert. This occurs when
    gpuservice starts up, causing gpuservice to crash continually in the
    background.
    
    The gpuWork program itself is not needed for any crucial functionality, and
    neither is gpuservice itself to be honest. That being said, gpuservice is a
    mainline module that is updated as part of Google Play system updates, and
    when any mainline modules continually crash after a Google Play system
    update, a reboot is triggered to rollback to the previous Google Play
    system update:
      12-21 22:47:01.633  1655 11184 I ShutdownThread: Rebooting, reason: Rollback staged install
    
    Hence, we need this so that the gpuWork eBPF program loads successfully.
    
    cp: https://github.com/kerneltoast/android_kernel_google_tensynos/commit/4f48227dbcad683f320de7666b554457fc2455ec
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -358,6 +358,7 @@ static const struct bpf_func_proto *bpf_get_probe_write_proto(void)
 BPF_CALL_5(bpf_trace_printk, char *, fmt, u32, fmt_size, u64, arg1,
 	   u64, arg2, u64, arg3)
 {
+#ifdef CONFIG_EVENT_TRACING
 	bool str_seen = false;
 	int mod[3] = {};
 	int fmt_cnt = 0;
@@ -466,6 +467,9 @@ BPF_CALL_5(bpf_trace_printk, char *, fmt, u32, fmt_size, u64, arg1,
 	      : __BPF_ARG2_TP((u32)arg3, ##__VA_ARGS__)))
 
 	return __BPF_TP_EMIT();
+#else
+	return 0;
+#endif
 }
 
 static const struct bpf_func_proto bpf_trace_printk_proto = {
@@ -478,12 +482,13 @@ static const struct bpf_func_proto bpf_trace_printk_proto = {
 
 const struct bpf_func_proto *bpf_get_trace_printk_proto(void)
 {
+#ifdef CONFIG_EVENT_TRACING
 	/*
 	 * this program might be calling bpf_trace_printk,
 	 * so allocate per-cpu printk buffers
 	 */
 	trace_printk_init_buffers();
-
+#endif
 	return &bpf_trace_printk_proto;
 }
 
@@ -1471,6 +1476,7 @@ static const struct bpf_func_proto bpf_read_branch_records_proto = {
 	.arg4_type      = ARG_ANYTHING,
 };
 
+#ifdef CONFIG_EVENT_TRACING
 static const struct bpf_func_proto *
 pe_prog_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 {
@@ -1489,6 +1495,7 @@ pe_prog_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 		return bpf_tracing_func_proto(func_id, prog);
 	}
 }
+#endif /* CONFIG_EVENT_TRACING */
 
 /*
  * bpf_raw_tp_regs are separate from bpf_pt_regs used from skb/xdp
@@ -1724,6 +1731,7 @@ const struct bpf_verifier_ops raw_tracepoint_writable_verifier_ops = {
 const struct bpf_prog_ops raw_tracepoint_writable_prog_ops = {
 };
 
+#ifdef CONFIG_EVENT_TRACING
 static bool pe_prog_is_valid_access(int off, int size, enum bpf_access_type type,
 				    const struct bpf_prog *prog,
 				    struct bpf_insn_access_aux *info)
@@ -1910,6 +1918,7 @@ int perf_event_query_prog_array(struct perf_event *event, void __user *info)
 	kfree(ids);
 	return ret;
 }
+#endif /* CONFIG_EVENT_TRACING */
 
 extern struct bpf_raw_event_map __start__bpf_raw_tp[];
 extern struct bpf_raw_event_map __stop__bpf_raw_tp[];
@@ -2029,6 +2038,7 @@ int bpf_probe_unregister(struct bpf_raw_event_map *btp, struct bpf_prog *prog)
 	return err;
 }
 
+#ifdef CONFIG_EVENT_TRACING
 int bpf_get_perf_event_info(const struct perf_event *event, u32 *prog_id,
 			    u32 *fd_type, const char **buf,
 			    u64 *probe_offset, u64 *probe_addr)
@@ -2075,6 +2085,7 @@ int bpf_get_perf_event_info(const struct perf_event *event, u32 *prog_id,
 
 	return err;
 }
+#endif /* CONFIG_EVENT_TRACING */
 
 #ifdef CONFIG_MODULES
 static int bpf_event_notify(struct notifier_block *nb, unsigned long op,
